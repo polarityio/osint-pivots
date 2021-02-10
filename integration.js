36474b9e8109904023ad9d88e874309007885337
Jon Penwood <jonp@polarity.io>
2021-02-10 12:36:37 -0600

    Updated some of the links and general cleanup.

--- integration.js
+++ integration.js
@@ -6,24 +6,20 @@ function startup(logger) {
 
 function doLookup(entities, options, cb) {
   let lookupResults = [];
-
+  
   entities.forEach(entity => {
-    let searchObject = [];
     lookupResults.push({
       entity: entity,
       data: {
-        summary: ['Expand to pivot our to external OSINT'],
-        details: {
-          searchObject: entity.object
-        }
+        summary: ['Expand to pivot out to external OSINT'],
+        details: entity.value.toLowerCase()
       }
     });
   });
-  Logger.trace({lookupResults:lookupResults}, "Logging lookup results");
   cb(null, lookupResults);
 }
 
 module.exports = {
   doLookup: doLookup,
-  startup:startup
+  startup: startup
 };
