cc4ea520d8062bc35651c480d87fe6827fa395de
Jon Penwood <jonp@polarity.io>
2023-10-18 11:29:25 -0500

    fix: Removed need to entity subtype

--- components/pivot-block.js
+++ components/pivot-block.js
@@ -1,4 +1,13 @@
-'use strict'
+'use strict';
 polarity.export = PolarityComponent.extend({
-  details: Ember.computed.alias('block.data.details')
+  details: Ember.computed.alias('block.data.details'),
+  isMsftkbEntity: false,
+  isAdobeEntity: false,
+  init() {
+    if (this.get('block.entity.type') === 'custom') {
+      if (this.get('block.entity.types').includes('custom.msftkb')) this.set('isMsftkbEntity', true);
+      if (this.get('block.entity.types').includes('custom.adobe')) this.set('isAdobeEntity', true);
+    }
+    this._super(...arguments);
+  }
 });
