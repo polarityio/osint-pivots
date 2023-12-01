2e37ddf79e6873af70f03696ac07d9685395079f
Ed <ed@polarity.io>
2023-11-30 22:32:10 -0500

    Fix computed

--- components/pivot-block.js
+++ components/pivot-block.js
@@ -1,10 +1,10 @@
 'use strict';
 polarity.export = PolarityComponent.extend({
   details: Ember.computed.alias('block.data.details'),
-  isMsftkbEntity: Ember.computed.alias('block.entity.types.[]', function () {
+  isMsftkbEntity: Ember.computed('block.entity.types.[]', function () {
     return this.get('block.entity.types').includes('custom.msftkb');
   }),
-  isAdobeEntity: Ember.computed.alias('block.entity.types.[]', function () {
+  isAdobeEntity: Ember.computed('block.entity.types.[]', function () {
     return this.get('block.entity.types').includes('custom.adobe');
   })
 });
