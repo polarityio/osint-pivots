4299dec3671883645299e8f07ce73604806e2d78
Ed <ed@polarity.io>
2025-03-27 19:05:23 -0400

    INT-1424: Fix Hash Lookups on v5

--- components/pivot-block.js
+++ components/pivot-block.js
@@ -1,10 +1,14 @@
 'use strict';
 polarity.export = PolarityComponent.extend({
-  details: Ember.computed.alias('block.data.details'),
-  isMsftkbEntity: Ember.computed('block.entity.types.[]', function () {
-    return this.get('block.entity.types').includes('custom.msftkb');
-  }),
-  isAdobeEntity: Ember.computed('block.entity.types.[]', function () {
-    return this.get('block.entity.types').includes('custom.adobe');
-  })
+    details: Ember.computed.alias('block.data.details'),
+    isMsftkbEntity: Ember.computed('block.entity.types.[]', function () {
+        return this.get('block.entity.types').includes('custom.msftkb');
+    }),
+    isAdobeEntity: Ember.computed('block.entity.types.[]', function () {
+        return this.get('block.entity.types').includes('custom.adobe');
+    }),
+    entityIsHash: Ember.computed('block.entity.type', function () {
+        let type = this.get('block.entity.type');
+        return type === 'hash' || type === 'MD5' || type === 'SHA1' || type === 'SHA256';
+    })
 });
